f121c9b82e6b0e42068477add77dfde023f7f4c7
Rohan Peri <perirohan@gmail.com>
2020-05-19 12:05:34 -0700

    adding starting files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+ import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App'// assumes .js automatically if you dont put anything else in file name
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+serviceWorker.unregister();
